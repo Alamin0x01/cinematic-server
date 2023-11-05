64638e29cd53eccb97b68c825466384a1daa7954
Md. Alamin A <86530759+Alamin0x01@users.noreply.github.com>
2023-11-05 09:24:02 +0600

    selected movies, get movies by email and update a movies -added

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function run() {
 
       res.send(result);
     });
-    //update a movie to handle refund policy
+    //update a movie to handle refund  policy
     app.put("/selectedmovies/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
